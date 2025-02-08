0190388f8c082789f52ea8a2fa3e181f0e64aeb1
Mark Wlodawski <imakethesites@gmail.com>
2025-02-08 11:09:39 -0500

    Add and style cover image, background responsive styling, and hovering images in front of Mark

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
-import { curve } from "../assets";
+import { curve, heroBackground } from "../assets";
 import Mark_Headshot from "../assets/Mark_Wlodawski_Headshot.jpeg";
 import Button from "./Button";
 import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
   return (
     <Section
-      className="pt-[12-rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 mx-auto text-center mb-[4rem] max-w-[52rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
             Hi, I&apos;m Mark Wlodawski
@@ -40,19 +47,48 @@ const Hero = () => {
           <div className="relative z-1 rounded-2xl p-0.5 bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
-              <div>
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={Mark_Headshot}
                   alt="robot"
-                  className="w-full"
-                  width={1440}
-                  height={1800}
+                  className="w-full scale-[1] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
                 />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex "
+                    title="Working with teams"
+                  />
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              alt="robot"
+              className="w-full"
+              width={1440}
+              height={1800}
+            />
+          </div>
+          <BackgroundCircles />
         </div>
       </div>
+      <BottomLine />
     </Section>
   );
 };
